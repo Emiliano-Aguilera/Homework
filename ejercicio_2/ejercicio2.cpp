98944ca950a698b977d3fd553a68d3568a52d88d
Emiliano Aguilera <eaguileramoreno@udesa.edu.ar>
2025-03-12 18:51:56 +0000

    actualizacion ejercicio 2

--- ejercicio_2/ejercicio2.cpp
+++ ejercicio_2/ejercicio2.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <format>
 #include <fstream>
+#include <stdexcept>
 
 void logMessage(std::string, int tag); // logMessage normal
 void logMessage(std::string message, std::string archivo, int linea); // logMessage error en linea de archivo
@@ -31,12 +32,11 @@ void logMessage(std::string message, int tag) {
         if (outFile.is_open()) {
             outFile << std::format("[{}] <{}>\n", tag_string, message);
         } else {
-            std::cerr << "Error abriendo archivo \n";
+            throw std::runtime_error("Error abriendo archivo");
         }
     } else {
-        std::string errorMessage = std::format(
-            "Intento de uso de logMessage invalido, TAG: {}, mensaje: {}", tag, message);
-        logMessage(errorMessage, ERROR);
+        std::string runtimeMessage = std::format("Tag invalido: {}, Mensaje: \"{}\"", tag, message);
+        throw std::runtime_error(runtimeMessage);
     }
 }
 
@@ -55,20 +55,31 @@ void logMessage(std::string message, std::string username){
 
 // Ejercicio IV
 int test_logMessage() {
+    try {
+        // Una entrada de cada tipo para logMessage (ejercicio a)
+        for (int i = DEBUG; i <= CRITICAL; i++) {
+            logMessage(std::format("{} test message.", tagStrings[i]), i);
+        }
+        // Una entrada de tipo ERROR que especifica la linea y el archivo (ejercicio b II)
+        logMessage("Syntax Error", "ejercicio_1.cpp", 25);
 
-    // Una entrada de cada tipo para logMessage (ejercicio a)
-    for (int i = DEBUG; i <= CRITICAL; i++) {
-        logMessage(std::format("{} test message.", tagStrings[i]), i);
+        // Una entrada de tipo Security que loggea el acceso de un usuario (ejercicio b III)
+        logMessage("Access Granted", "emiag");
+
+        // Una entrada con un tipo invalido, genera un log tipo ERROR
+        logMessage("Mensaje con error", 666);
+
+        // Mensaje de log valido que nunca se va a ejecutar
+        logMessage("Mensaje valido", WARNING);
+        
+        return 0;
+    } 
+    catch (std::runtime_error& e) {
+        logMessage(e.what(), ERROR);
+        return 1;
+    } 
+    catch (...) {
+        logMessage("Algo salio mal, error desconocido", ERROR);
+        return 1;
     }
-
-    // Una entrada con un tipo invalido, genera un log tipo ERROR
-    logMessage("Mensaje con error", 666);
-
-    // Una entrada de tipo ERROR que especifica la linea y el archivo (ejercicio b II)
-    logMessage("Syntax Error", "ejercicio_1.cpp", 25);
-
-    // Una entrada de tipo Security que loggea el acceso de un usuario (ejercicio b III)
-    logMessage("Access Granted", "emiag");
-
-    return 0;
 }
