7394f56be098b18bf39a7d1e417c359d98ee5896
Emiliano Aguilera <eaguileramoreno@udesa.edu.ar>
2025-03-12 01:32:27 +0000

    Solucion temporal del ejercicio 3. Queda pendiente dejar de usar variables globales Investigar alguna forma mas elegante de manejar las variables, tal vez usando clases. Revisar uso correcto de shared pointer, tal vez alguno puede ser unique.

---
+++ ejercicio_3/ejercicio_3.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <memory>
+
+struct Node {
+    int value {};
+    std::shared_ptr<Node> next;
+};
+
+std::shared_ptr<Node> primero;
+
+std::shared_ptr<Node> create_node(int value);
+void push_front(int value);
+void push_back(int value);
+void insert(int value, int position);
+void print_list();
+
+int countNodes();
+
+int main() {
+    primero = create_node(0);
+    
+    push_front(1);
+    push_front(2);
+
+    print_list();
+
+    push_back(4);
+    push_back(5);
+    print_list();
+
+    insert(9, 2);
+    print_list();
+
+    insert(99, 4);
+    print_list();
+
+    insert(100, 7);
+    print_list();
+
+
+    return 0;
+}
+
+std::shared_ptr<Node> create_node(int value) {
+    auto nodo = std::make_shared<Node>();
+    nodo->value = value;
+    nodo->next = nullptr;
+    return nodo;
+}
+
+void push_front(int value) {
+    std::shared_ptr<Node> nodo = create_node(value);
+    nodo->next = primero;
+    primero = nodo;
+}
+
+void push_back(int value) {
+    std::shared_ptr<Node> nodo = create_node(value);
+    std::shared_ptr<Node> actual = primero;
+
+    while (actual->next != nullptr) {
+        actual = actual->next;
+    }
+
+    actual->next = nodo;
+}
+
+void insert(int value, int position) {
+    int nodeCount = countNodes();
+
+    if (nodeCount <= position) {
+        std::cerr << "Posicion invalida, insertando nodo al final." << std::endl;
+        push_back(value);
+    } else {
+        std::shared_ptr<Node> nodo = create_node(value);
+        std::shared_ptr<Node> actual = primero;
+
+        for (int i = 0; i < position - 1; i++) {
+            actual = actual->next;
+        }
+        nodo->next = actual->next;
+        actual->next = nodo;
+    }
+}   
+ 
+void print_list() {
+    std::shared_ptr<Node> actual = primero;
+
+    while (actual != nullptr) {
+        std::cout << actual->value << " -> ";
+        actual = actual->next;
+    }
+    std::cout << "null" << std::endl;
+}
+
+int countNodes() {
+    std::shared_ptr<Node> actual = primero;
+    int nodeCount {};
+
+    while (actual != nullptr) {
+        actual = actual->next;
+        nodeCount++;
+    }
+
+    return nodeCount;
+}
